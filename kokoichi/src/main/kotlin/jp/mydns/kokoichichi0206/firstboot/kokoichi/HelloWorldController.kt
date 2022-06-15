a347cc61d01476cc1e6b372bebea8511092f5967
takahiro.tominaga <tt.300607@gmail.com>
2022-06-15 21:52:23 +0900

    first api endpoint

---
+++ kokoichi/src/main/kotlin/jp/mydns/kokoichichi0206/firstboot/kokoichi/HelloWorldController.kt
@@ -0,0 +1,15 @@
+package jp.mydns.kokoichichi0206.firstboot.kokoichi
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld(): String {
+        return "Hello, this is a REST endpoint!"
+    }
+}
