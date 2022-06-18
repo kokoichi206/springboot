27dd2c9af2d5b5cf2140f0173b828b92d979a1f2
takahiro.tominaga <tt.300607@gmail.com>
2022-06-18 11:25:06 +0900

    ormapper initial commit

---
+++ ormapper/src/main/kotlin/com/example/ormapper/OrmapperApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ormapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrmapperApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrmapperApplication>(*args)
+}
